ab6b71f559886df45f8d061201f764da01142c7e
SABRINA CORREIA <sabrinacorreia760@gmail.com>
2025-09-16 09:42:41 -0400

    feat: :sparkles: partial unique indexes to manage soft deletion and citext columns for email and username in user

--- services/identity-service/src/main/java/com/tickon/identityservice/user/infrastructure/persistence/jpa/JpaUserEntity.java
+++ services/identity-service/src/main/java/com/tickon/identityservice/user/infrastructure/persistence/jpa/JpaUserEntity.java
@@ -2,6 +2,9 @@ package com.tickon.identityservice.user.infrastructure.persistence.jpa;
 
 import java.time.Instant;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -9,11 +12,19 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
+@SQLDelete(sql = "UPDATE users SET is_deleted = true, deleted_at = now() WHERE id = ?")
+@SQLRestriction("is_deleted = false")
 public class JpaUserEntity {
   @Id public String id;
 
-  @Column(name = "username", unique = true)
+  @Column(name = "username", nullable = false, columnDefinition = "citext")
   public String username;
+
+  @Column(name = "email", nullable = false, columnDefinition = "citext")
+  public String email;
+
+  @Column(name = "password_hash")
+  public String passwordHash;
 
   @Column(name = "first_name")
   public String firstName;
@@ -21,23 +32,17 @@ public class JpaUserEntity {
   @Column(name = "last_name")
   public String lastName;
 
-  @Column(name = "email", unique = true, nullable = false)
-  public String email;
-
-  @Column(name = "password_hash")
-  public String passwordHash;
-
   @Column(name = "created_at")
   public Instant createdAt;
 
   @Column(name = "updated_at")
   public Instant updatedAt;
 
-  @Column(name = "is_deleted")
-  public boolean isDeleted;
+  @Column(name = "is_deleted", nullable = false)
+  public boolean isDeleted = false;
 
   @Column(name = "deleted_at")
   public Instant deletedAt;
 
-  public JpaUserEntity() {}
+  protected JpaUserEntity() {}
 }

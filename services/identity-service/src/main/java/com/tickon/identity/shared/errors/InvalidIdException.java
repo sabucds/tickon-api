c31ce13b704e62ae1921d23612f794c054c3b155
SABRINA CORREIA <sabrina.correia@correo.unimet.edu.ve>
2026-01-13 08:29:25 -0400

    refactor: :recycle: now database migrations are manual for granular control

--- services/identity-service/src/main/java/com/tickon/identity/shared/errors/InvalidIdException.java
+++
@@ -1,7 +0,0 @@
-package com.tickon.identity.shared.errors;
-
-public class InvalidIdException extends DomainException {
-  public InvalidIdException(String message) {
-    super(ErrorCode.INVALID_ID, message);
-  }
-}

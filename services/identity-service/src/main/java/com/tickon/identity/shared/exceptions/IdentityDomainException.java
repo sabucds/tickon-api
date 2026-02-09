11f0199d026be21f9f88cff489ff096ff1001d47
SABRINA CORREIA <sabrina.correia@correo.unimet.edu.ve>
2026-02-09 11:43:52 -0400

    refactor: :fire: unused common DomainException

--- services/identity-service/src/main/java/com/tickon/identity/shared/exceptions/IdentityDomainException.java
+++ services/identity-service/src/main/java/com/tickon/identity/shared/exceptions/IdentityDomainException.java
@@ -1,8 +1,6 @@
 package com.tickon.identity.shared.exceptions;
 
-import com.tickon.common.exceptions.DomainException;
-
-public abstract class IdentityDomainException extends DomainException {
+public abstract class IdentityDomainException extends RuntimeException {
   private final IdentityExceptionCodes code;
 
   protected IdentityDomainException(IdentityExceptionCodes code, String message) {

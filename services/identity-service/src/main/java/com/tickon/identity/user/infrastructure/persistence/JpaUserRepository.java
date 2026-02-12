c192b304d2689f31ad414c8ee11b30fd419c2b61
SABRINA CORREIA <sabrina.correia@correo.unimet.edu.ve>
2026-02-12 14:25:06 -0400

    mvn spotless:apply

--- services/identity-service/src/main/java/com/tickon/identity/user/infrastructure/persistence/JpaUserRepository.java
+++ services/identity-service/src/main/java/com/tickon/identity/user/infrastructure/persistence/JpaUserRepository.java
@@ -1,10 +1,9 @@
 package com.tickon.identity.user.infrastructure.persistence;
 
+import com.tickon.identity.user.infrastructure.persistence.entities.UserEntity;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tickon.identity.user.infrastructure.persistence.entities.UserEntity;
 
 public interface JpaUserRepository extends JpaRepository<UserEntity, UUID> {
   boolean existsByEmail(String email);
